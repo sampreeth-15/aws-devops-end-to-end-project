dfba69253b146d4b53749f93b5102cafeee6cd41
sampreeth B S <sampreethsampu15@gmail.com>
2026-02-12 09:15:18 +0530

    Add outputs for EC2 instance public IP and ID

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,10 @@
+output "instance_public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.web.public_ip
+}
+
+output "instance_id" {
+  description = "EC2 Instance ID"
+  value       = aws_instance.web.id
+}
 
