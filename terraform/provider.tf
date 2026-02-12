9010aeffce2f028bc01bf4fd1a3b2770c2131540
sampreeth B S <sampreethsampu15@gmail.com>
2026-02-12 09:20:16 +0530

    Add AWS provider and S3 backend configuration

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,1 +1,13 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "sampreeth-terraform-state-123"
+    key            = "devops-project/terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+  }
+}
 
